b1545d356290e9ea19f7146a64689b9964693857
bart1019 <bartlomiej.runowski@gmail.com>
2021-04-27 20:51:21 +0200

    Schedule page finished with some initial js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+//Home Page 
+
 const wrapper = document.querySelector(".wrapper");
 const hamburgerMenu = document.querySelector("ul i");
 
 hamburgerMenu.addEventListener('click', function() {
     wrapper.classList.toggle("off");
+})
+
+//Schedules Page
+const arrivals = document.querySelector(".tableHeader div:last-of-type");
+const departures = document.querySelector(".tableHeader div:first-of-type");
+var arrivalsTable = document.querySelectorAll(".arrivals");
+var departuresTable = document.querySelectorAll(".departures");
+
+arrivals.addEventListener('click', function() {   
+    arrivalsTable.forEach(element => {
+        element.classList.toggle("off");
+    });
+})
+
+departures.addEventListener('click', function() {
+    departuresTable.forEach(element => {
+        element.classList.toggle("off");
+    });
 })
